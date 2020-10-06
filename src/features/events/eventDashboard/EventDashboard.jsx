a268851db133cd8fdb73f228888c518d9a240913
P.C Ndayisenga <pacifiqueclement@gmail.com>
2020-10-06 16:37:52 +0200

    Style Events list and event list item

--- src/features/events/eventDashboard/EventDashboard.jsx
+++ src/features/events/eventDashboard/EventDashboard.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Grid } from 'semantic-ui-react';
 
+import EventList from './EventList';
+
 const EventDashboard = () => {
   return (
     <Grid>
-      <Grid.Column width={8}>
-        <h1>Hello there</h1>
+      <Grid.Column width={10}>
+        <EventList />
       </Grid.Column>
-      <Grid.Column width={8}>
+      <Grid.Column width={6}>
         <h1>Hello here</h1>
       </Grid.Column>
     </Grid>
---
+++ src/features/events/eventDashboard/EventList.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import EventListItem from './EventListItem';
+
+const EventList = () => {
+  return (
+    <div>
+      <EventListItem />
+      <EventListItem />
+      <EventListItem />
+      <EventListItem />
+      <EventListItem />
+      <EventListItem />
+    </div>
+  );
+};
+
+export default EventList;
